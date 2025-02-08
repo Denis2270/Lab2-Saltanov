4d54df465141e98f7bf424665a9c4c9b305a31ea
Saltanov <den050319@gmail.com>
2025-02-08 16:33:56 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowSaltanovD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFirstWindow(object sender, RoutedEventArgs e)
+        {
+            var firstWindow = new FirstWindow();
+            firstWindow.Show();
+        }
+
+        private void OpenSecondWindow(object sender, RoutedEventArgs e)
+        {
+            var secondWindow = new SecondWindow();
+            secondWindow.ShowDialog();
+        }
+
+        private void OpenThirdWindow(object sender, RoutedEventArgs e)
+        {
+            var thirdWindow = new ThirdWindow();
+            App.Current.MainWindow = thirdWindow;
+            thirdWindow.Show();
+            Close();
+        }
+    }
+}
---
+++ ThirdWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowSaltanovD
+{
+    /// <summary>
+    /// Логика взаимодействия для ThirdWindow.xaml
+    /// </summary>
+    public partial class ThirdWindow : Window
+    {
+        public ThirdWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseThirdWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
